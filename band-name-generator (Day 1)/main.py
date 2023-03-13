583b4226066540fa33f3cb6ca0eefbb582216e91
thearasu <startwithel@gmail.com>
2023-03-13 19:14:03 +0530

    Day 1

---
+++ band-name-generator (Day 1)/main.py
@@ -0,0 +1,4 @@
+print("Welcome to brand name generator!")
+city_name = input("Enter your city name:\n")
+pet_name = input("Enter your pet name:\n")
+print("Your band name is: " + city_name + " " + pet_name)
